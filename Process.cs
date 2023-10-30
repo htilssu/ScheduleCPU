5f5426693abdb3c137c9b6a8a05857bcbcf3710e
htilssu <tolashuu@gmail.com>
2023-10-30 17:27:58 +0700

    update: set default priority to 0

--- Process.cs
+++ Process.cs
@@ -5,6 +5,6 @@
         public string ProcessName { get; set; }
         public int ArrivalTime { get; set; }
         public int BurstTime { get; set; }
-        public int? Priority { get; set; }
+        public int? Priority { get; set; } = 0;
     }
 }
